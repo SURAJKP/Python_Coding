f620961a55d81107ca0289819453187c28b681d0
SURAJ PATIL <37582161+SURAJKP@users.noreply.github.com>
2025-11-30 20:06:46 +0530

    two sum problem solved with HashMap

--- twoSum.py
+++ twoSum.py
@@ -1,8 +1,22 @@
-num1 = 10
-num2 = 20
+# nums = [2,7,11,15], target = 9
+# output = [0,1]    # Because 2 + 7 = 9
 
-def two_sum(a, b):
-    return a + b
+# array is a normal list behaves like a dynamic array
+#normal key operations are delete, insert, update, search
 
-result = two_sum(num1, num2)
-print ("the sum is:", result)
+def twoSum(nums, target):
+    num_map = {}  # Dictionary to store the complement and its index
+
+    for index, num in enumerate(nums):
+        complement = target - num
+        if complement in num_map:
+            return [num_map[complement], index]
+        num_map[num] = index
+ 
+    return []  # Return an empty list if no solution is found
+
+# Example usage:
+nums = [2, 7, 11, 15]
+target = 9
+result = twoSum(nums, target)
+print(result)  # Output: [0, 1]  
