989ebf5f1022a2169cde4bb0d3e7030d43ef488c
SURAJ PATIL <37582161+SURAJKP@users.noreply.github.com>
2026-01-25 16:42:55 +0530

    JS solution for Twosum

--- twoSum.py
+++ twoSum.py
@@ -20,3 +20,15 @@ nums = [2, 7, 11, 15]
 target = 9
 result = twoSum(nums, target)
 print(result)  # Output: [0, 1]  
+
+# Javascript solution
+# var twoSum = function(nums, target) {
+#     for (let i=0; i<nums.length-1; i++) {
+#         for (let j=i+1; j<nums.length; j++) {
+#             let sum = nums[i] + nums[j];
+#             if (sum == target) {
+#             return [i, j]
+#             }
+#         }
+#     }
+# };
